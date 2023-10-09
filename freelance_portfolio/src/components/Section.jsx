fc1b71dee18473da1fa9ad961bc4026ce38e3102
satele99 <satele.works@gmail.com>
2023-10-09 14:03:37 -0400

    css animations

--- freelance_portfolio/src/components/Section.jsx
+++ freelance_portfolio/src/components/Section.jsx
@@ -1,4 +1,5 @@
 import styles from '../styles/Section.module.css'
+import images from '../images/IMG_6292.jpg'
 
 export default function Section() {
     return (
@@ -21,10 +22,20 @@ export default function Section() {
                     </div>
                 </div>
                 <div className={styles.content}>
-                    <div>
-                        <h1>Image</h1>
+                    <div className={styles.img_wrapper}>
+                        <img src={images}/>
                     </div>
+                    
                 </div>
+                <svg className={styles.design_element} style={{top: '13em', right: '11em' , transform: 'rotate(180deg)'}} viewBox="0 0 1194 1194" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M596.542 1193.08C486.693 795.367 397.718 706.392 9.59946e-05 596.542C397.718 486.693 486.761 397.718 596.543 -2.60757e-05C706.392 397.718 795.367 486.693 1193.08 596.542C795.367 706.392 706.392 795.367 596.542 1193.08Z" fill="#FFD439"/>
+                </svg>
+                <svg className={styles.design_element} style={{top: '38em', right: '15em' , transform: 'rotate(37deg)'}} viewBox="0 0 1194 1194" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M596.542 1193.08C486.693 795.367 397.718 706.392 9.59946e-05 596.542C397.718 486.693 486.761 397.718 596.543 -2.60757e-05C706.392 397.718 795.367 486.693 1193.08 596.542C795.367 706.392 706.392 795.367 596.542 1193.08Z" fill="#FFD439"/>
+                </svg>
+                <svg className={styles.design_element} style={{top: '22em', right: '-.5em', transform: 'rotate(15deg)'}} viewBox="0 0 1194 1194" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M596.542 1193.08C486.693 795.367 397.718 706.392 9.59946e-05 596.542C397.718 486.693 486.761 397.718 596.543 -2.60757e-05C706.392 397.718 795.367 486.693 1193.08 596.542C795.367 706.392 706.392 795.367 596.542 1193.08Z" fill="#FFD439"/>
+                </svg>
             </div>
         </section>
     )
